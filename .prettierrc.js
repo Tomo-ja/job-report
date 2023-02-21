c690c643fd118964ef30570c9d9ebe69a554e818
Tomo-ja <tomonao.hj@gmail.com>
2023-02-21 12:37:54 -0800

    husky, prettier, and lint setting

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: 'auto',
+    arrowParens: 'avoid',
+    trailingCommaL: 'none',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+};
